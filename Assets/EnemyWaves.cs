50bc03d49295cb1da2c7d7215745f88fb0f338c2
Salamander333 <y.glushkov@abv.bg>
2019-01-26 14:12:35 +0200

    wave_spawner

---
+++ Assets/EnemyWaves.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaves : MonoBehaviour {
+
+    public WaveData waveData;
+    private IEnumerable waveCoroutine;
+    public float timeBetweenWaves;
+    public int waveCount;
+
+	void Start () {
+        StartCoroutine(waveData.Wave(timeBetweenWaves, waveCount));
+	}
+	
+	void Update () {
+		
+	}
+
+}
