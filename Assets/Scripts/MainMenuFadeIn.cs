262bb5370a45c09a6cf7d2fc60fa5dffabee1309
Salamander333 <y.glushkov@abv.bg>
2019-01-26 20:35:50 +0200

    main_menu_update

---
+++ Assets/Scripts/MainMenuFadeIn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuFadeIn : MonoBehaviour {
+
+    public Material blurMaterial;
+    float blurValue = 5;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (blurValue > 0)
+        {
+            blurValue -= 0.04f;
+            blurMaterial.SetFloat("_Size", blurValue);
+        }
+        
+	}
+}
