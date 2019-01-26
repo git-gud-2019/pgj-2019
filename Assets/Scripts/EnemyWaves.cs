71b9a6df11b0eb99cb5432beed86b93ea282b6dc
Salamander333 <y.glushkov@abv.bg>
2019-01-26 13:05:24 +0200

    feat/wavespawning

---
+++ Assets/Scripts/EnemyWaves.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaves : MonoBehaviour {
+
+    public WaveData waveData;
+
+	void Start () {
+        StartCoroutine(waveData.Wave(waveData.timeBetweenWaves));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/WaveData.cs
+++ Assets/Scripts/WaveData.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "WaveData", menuName = "Waves/Data", order = 1)]
-public class WaveData : ScriptableObject {
+public class WaveData : ScriptableObject
+{
 
     [SerializeField]
     public Transform[] enemies;
     public int cicle;
+    public int enemiesPerWave;
     public float spawnInterval;
+    public float timeBetweenWaves;
+    public List<GameObject> path;
+
+
+    public IEnumerator Wave(float timeBetweenWaves)
+    {
+        for (var i = 0; i < enemiesPerWave; i++)
+        {
+            var enemyGO = enemies[Random.Range(0, 1)];
+            var go = Instantiate(enemyGO, GameObject.FindGameObjectWithTag("SpawnPoint").transform.position, Quaternion.identity);
+            go.GetComponent<Enemy>().path = path;
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
 }
