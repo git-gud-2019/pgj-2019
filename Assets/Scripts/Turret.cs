f216110780a743c8be140844621271434bff9e31
TabakMan <assassin8123@gmail.com>
2019-01-26 18:15:23 +0200

    fix(turret): aiming

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
@@ -64,7 +62,7 @@ public class Turret : MonoBehaviour
         {
             lookRotationX = rotation.x * -1 + 90;
         }
-        transform.rotation = Quaternion.Euler(lookRotationX + 270, 270f, 90f);
+        transform.rotation = Quaternion.Euler(lookRotationX, 270f, 90f);
 
     }
 
