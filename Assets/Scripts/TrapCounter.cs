23a1c71be9df9268a302b48fa2c7b4ebefb3b502
TabakMan <assassin8123@gmail.com>
2019-01-26 02:50:31 +0200

    feat(hud): add hud counter mehcanisum

---
+++ Assets/Scripts/TrapCounter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TrapCounter : MonoBehaviour {
+
+    public Image TrapPrefab;
+    public GameObject SpawnPoint;
+    private int Traps = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        InstantiateLives();
+    }
+
+    private void InstantiateLives()
+    {
+        float spaceMultiplier = 50f;
+        for (int i = 0; i < Traps; i++)
+        {
+            var trap = Instantiate(TrapPrefab,
+                new Vector3(SpawnPoint.transform.position.x + spaceMultiplier * i, SpawnPoint.transform.position.y,
+                    0f), Quaternion.identity);
+            trap.transform.SetParent(SpawnPoint.transform);
+            trap.tag = "Traps";
+        }
+    }
+
+    private void DestroyLives()
+    {
+        var latestLive = GameObject.FindGameObjectsWithTag("Traps");
+        if (latestLive.Length > 0)
+        {
+            foreach (var live in latestLive)
+            {
+                Destroy(live);
+            }
+        }
+    }
+
+    public void ChangeLives(int change)
+    {
+        Traps = Traps + change;
+        DestroyLives();
+        InstantiateLives();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
