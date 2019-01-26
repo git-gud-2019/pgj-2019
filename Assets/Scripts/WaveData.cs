d6991042fb6d510c6c22dcb6d0dfa21f6612b60d
gkostov0101 <askaron951@gmail.com>
2019-01-26 11:38:29 +0200

    work so far

---
+++ Assets/Scripts/WaveData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WaveData", menuName = "Waves/Data", order = 1)]
+public class WaveData : ScriptableObject {
+
+    [SerializeField]
+    public Transform[] enemies;
+    public int cicle;
+    public float spawnInterval;
+}
